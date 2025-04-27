8eb628e71f063a228abc5539520461d9d468ba46
danil <danil>
2025-04-27 10:16:43 +0700

    фикс

--- menu/build/menu/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '514bbea4d33e2283069f');
--- menu/build/menu/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '24c765a3bbf83e2f5974');
